efbca21d480732c136c609cfca34b982cef0814b
Emily Haynes <ehayne00@hotmail.com>
2019-10-01 19:33:52 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index {|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map{|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  counter = 0
+  while counter < array.length do
+    return true if array[counter].length>4
+    counter += 1
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+ cheese_types.map{|cheese| return cheese if array.include?(cheese)}.include?(true)
+ puts cheese_types - array
 end
